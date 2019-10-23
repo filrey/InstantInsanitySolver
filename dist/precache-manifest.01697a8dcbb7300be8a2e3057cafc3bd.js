ece7af657654a592b24757558ab16fe7aae1d8f6
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-10-23 11:46:22 -0700

    adding dist

---
+++ dist/precache-manifest.01697a8dcbb7300be8a2e3057cafc3bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b13fb81bde93ae7ef828",
+    "url": "/js/chunk-vendors.fdcb9653.js"
+  },
+  {
+    "revision": "3681640b0fcfee9514f0",
+    "url": "/js/app.2cee1576.js"
+  },
+  {
+    "revision": "c5b5abdcc4dc239d4fde",
+    "url": "/js/about.68b09bec.js"
+  },
+  {
+    "revision": "50c4211376f8730025abaf4bb2e2dcfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13fb81bde93ae7ef828",
+    "url": "/css/chunk-vendors.93aaabb8.css"
+  },
+  {
+    "revision": "3681640b0fcfee9514f0",
+    "url": "/css/app.5f024abc.css"
+  },
+  {
+    "revision": "c5b5abdcc4dc239d4fde",
+    "url": "/css/about.d256b758.css"
+  }
+];
